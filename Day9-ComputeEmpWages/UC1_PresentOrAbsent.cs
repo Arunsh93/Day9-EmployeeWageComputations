a1fe681f31eb2ea9f80f30f544e82e54dab434b8
Arunsh93 <Arunhosur93@gmail.com>
2021-09-27 17:26:10 +0530

    Checking Employee attendnce Using Random

---
+++ Day9-ComputeEmpWages/UC1_PresentOrAbsent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day9_ComputeEmpWages
+{
+    class UC1_PresentOrAbsent
+    {
+        public void CheckEmp()
+        {
+            int is_Full_Time = 1;
+            Random random = new Random();
+            int result = random.Next(0, 2);
+
+            if (result == is_Full_Time)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
