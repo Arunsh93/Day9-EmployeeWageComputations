e6bb6ee5528ccd43fe7204f9de8f8a22b7460a85
Arunsh93 <Arunhosur93@gmail.com>
2021-09-27 17:22:13 +0530

    Welcome message Day9 Employee Wages Computations

---
+++ Day9-ComputeEmpWages/MainMethodClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day9_ComputeEmpWages
+{
+    class MainMethodClass
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Day9 Employee Wages Computations");
+        }
+    }
+}
